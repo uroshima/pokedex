110060dc3f0638b1c6c903a716e1d4923b431ab8
urosh <urosh.stojkovikj@gmail.com>
2019-01-09 17:07:30 -0800

    created frontend folder with all the subfolders

---
+++ app/frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rootEl = document.getElementById("root");
+    ReactDom.render(<h1>Jaj me</h1>, rootEl);
+});
