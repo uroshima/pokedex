579b40f9750c5aed00e4294e87e2960ccb6fb046
urosh <urosh.stojkovikj@gmail.com>
2019-01-17 17:32:12 -0800

    created create metod in the pokemon controller

--- app/controllers/api/pokemons_controller.rb
+++ app/controllers/api/pokemons_controller.rb
@@ -3,6 +3,16 @@ class Api::PokemonController < ApplicationController
         @pokemon = Pokemon.all 
     end 
 
+    def create
+        @pokemon = Pokemon.new(pokemon_params)
+        
+        if @pokemon.save!
+        render :show
+        else
+        render json: ["Pokemon can't be created"], status: 422
+        end
+    end
+
     def show 
         @pokemon = Pokemon.find(params[:id])
         if @pokemon
@@ -11,4 +21,9 @@ class Api::PokemonController < ApplicationController
             render json: ["Pokemon doen't exist"], status: 404
         end
     end
+
+    def pokemon_params
+        params.require(:pokemon).permit(:attack, :defense, :image_url, :name, :poke_type)
+    end
 end
+
