d84191eff1dba18877e01014dd764021b5528b41
urosh <urosh.stojkovikj@gmail.com>
2019-01-17 17:54:17 -0800

    created pokemon_form_container

---
+++ app/frontend/components/pokemon/pokemon_form_container.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+import PokemonForm from "./pokemon_form";
+import { createPokemon } from "../../actions/pokemon_actions";
+
+const mapDispatchToProps = dispatch => ({
+    createPokemon: (pokemon) => dispatch(createPokemon(pokemon))
+});
+
+export default connect(
+  mapDispatchToProps
+)(PokemonForm);
